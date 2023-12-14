4012224e11520bceb46925ac2b294a28252eb16c
David Vávra <david@vavra.me>
2023-12-14 22:28:05 +0100

    Share sheet

--- app/src/main/java/me/vavra/newpickers/MainActivity.kt
+++ app/src/main/java/me/vavra/newpickers/MainActivity.kt
@@ -1,14 +1,19 @@
 package me.vavra.newpickers
 
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Intent
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.service.chooser.ChooserAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -20,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import me.vavra.newpickers.ui.theme.NewPickersTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +48,13 @@ class MainActivity : ComponentActivity() {
                             Text(text = "Pick image (new)")
                         }
                         Text(text = "Selected image: $selectedImageUri")
+                        Spacer(modifier = Modifier.padding(20.dp))
+                        Button(onClick = { shareLinkOld() }) {
+                            Text(text = "Share link (old)")
+                        }
+                        Button(onClick = { shareLinkNew() }) {
+                            Text(text = "Share link (new)")
+                        }
                     }
                 }
             }
@@ -66,6 +79,45 @@ class MainActivity : ComponentActivity() {
 
     private fun pickImageNew() {
         pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun shareLinkOld() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "https://gdg.community.dev/events/details/google-gdg-bishkek-presents-devfest-bishkek-2023/")
+            type = "text/plain"
+        }
+        startActivity(sendIntent)
+    }
+
+    private fun shareLinkNew() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "https://gdg.community.dev/events/details/google-gdg-bishkek-presents-devfest-bishkek-2023/")
+            putExtra(Intent.EXTRA_TITLE, "DevFest Bishkek 2023")
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null).apply {
+            putExtra(Intent.EXTRA_INITIAL_INTENTS, listOf(intent).toTypedArray())
+            val excludedComponentNames = arrayOf(ComponentName("com.google.android.apps.docs", "com.google.android.apps.docs.common.shareitem.UploadMenuActivity"))
+            putExtra(Intent.EXTRA_EXCLUDE_COMPONENTS, excludedComponentNames)
+        }
+        if (Build.VERSION.SDK_INT >= 34) {
+            val customActions = arrayOf(
+                ChooserAction.Builder(
+                    Icon.createWithResource(this, androidx.core.R.drawable.ic_call_answer),
+                    "Call",
+                    PendingIntent.getBroadcast(
+                        this,
+                        1,
+                        Intent(Intent.ACTION_VIEW),
+                        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_CANCEL_CURRENT
+                    )
+                ).build()
+            )
+            shareIntent.putExtra(Intent.EXTRA_CHOOSER_CUSTOM_ACTIONS, customActions)
+        }
+        startActivity(shareIntent)
     }
 
 }
